a2dda8ff4aaa17a867b4a256301d3baab45600b6
FELIX-MAIN <felix.irungu@student.moringaschool.com>
2023-03-14 03:15:39 +0300

    Done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,14 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def grades
+    result = Student.find(params[:grades])
+    render json: result
+  end
+
+  def highest_grade
+    result = Student.find(params[:highest-grade])
+    render json: result
+  end
+
 end
